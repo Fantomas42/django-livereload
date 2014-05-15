754041952696d7c3237febe9d6842609450dfa2a
Fantomas42 <fantomas42@gmail.com>
2014-05-15 17:32:42 +0200

    LiveReload != livereload

--- livereload/management/commands/runserver.py
+++ livereload/management/commands/runserver.py
@@ -16,17 +16,17 @@ else:
 
 class Command(RunserverCommand):
     """
-    Command for running the development server with Livereload.
+    Command for running the development server with LiveReload.
     """
     option_list = RunserverCommand.option_list + (
         make_option('--nolivereload', action='store_false',
                     dest='use_livereload', default=True,
-                    help='Tells Django to NOT use Livereload.'),
+                    help='Tells Django to NOT use LiveReload.'),
         make_option('--livereload-port', action='store',
                     dest='livereload_port', default='35729',
-                    help='Port where Livereload listen.'),
+                    help='Port where LiveReload listen.'),
     )
-    help = 'Starts a lightweight Web server for development with Livereload.'
+    help = 'Starts a lightweight Web server for development with LiveReload.'
 
     def message(self, message, verbosity=1, style=None):
         if verbosity:
@@ -36,21 +36,21 @@ class Command(RunserverCommand):
 
     def livereload_request(self, **options):
         """
-        Performs the livereload request.
+        Performs the LiveReload request.
         """
         style = color_style()
         verbosity = int(options['verbosity'])
         host = 'localhost:%s' % options['livereload_port']
         try:
             urllib.urlopen('http://%s/changed?files=.' % host)
-            self.message('LiveReload request emitted.\n !!', verbosity)
+            self.message('LiveReload request emitted.\n',
+                         verbosity, style.HTTP_INFO)
         except IOError:
-            self.message('> LiveReload server unreachable at %s' % host,
-                         verbosity, style.HTTP_BAD_REQUEST)
+            pass
 
     def get_handler(self, *args, **options):
         """
-        Entry point to plug the livereload feature.
+        Entry point to plug the LiveReload feature.
         """
         handler = super(Command, self).get_handler(*args, **options)
         self.livereload_request(**options)
